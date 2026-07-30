fe1740d00a15349add70dcec3d5a753eab9580c6
NydiaOlmos <nydia.naomi@gamil.com>
2026-07-30 17:46:07 -0600

    Refactor: Modularización del manejo de la base de datos

--- backend/main.py
+++ backend/main.py
@@ -2,138 +2,71 @@
 # fastapi dev
 
 # Manejo de las peticiones a la api
-from fastapi import FastAPI
-
-# Conexion con la db
-import sqlalchemy as db
-import os
+from fastapi import FastAPI, Depends, HTTPException
 
 # Manejo de la base de datos
-from datetime import datetime
-from enum import Enum
-from typing import List, Optional
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+# Importa la sesión y los modelos
+from database import SessionLocal, engine
+from models import *
 
 
-# ------- Variables de entorno para evitar el hardcode -------
-host = os.environ['DB_HOST']
-pwd = os.environ['DB_PASSWORD']
-usuario  = os.environ['DB_USER']
-
-
-# ------- Creacion de la estructura de la base de datos -------
-class Base(DeclarativeBase):
-    pass
-
-class Suelo(Enum):
-    ARENOSO = "arenoso"
-    CALIZO = "calizo"
-    TIERRA_NEGRA = "tierra negra"
-    ARCILLOSO = "arcilloso"
-    OTRO = "otro"
-
-class Planta(Base):
-    __tablename__ = "plantas"
-
-    id_planta: Mapped[int] = mapped_column(primary_key=True)
-    nombre: Mapped[str] = mapped_column(db.String(50))
-    tipo: Mapped[str] = mapped_column(db.String(50))
-    tipo_suelo: Mapped[Suelo] = mapped_column(db.Enum(Suelo, native_enum=False))
-
-    metricas: Mapped[List["Metrica"]] = relationship(back_populates="planta", cascade="all, delete-orphan")
-
-    def __repr__(self):
-        return f"""Planta(
-        id={self.id_planta!r}, 
-        nombre={self.nombre!r}, 
-        tipo={self.tipo!r}, 
-        tipo_suelo={self.tipo_suelo!r}
-        )"""
-
-class Metrica(Base):
-    __tablename__ = "metricas"
-
-    id_metrica: Mapped[int] = mapped_column(primary_key=True)
-    id_planta: Mapped[int] = mapped_column(db.ForeignKey("plantas.id_planta"))
-    humedad_suelo: Mapped[Optional[float]]
-    temperatura_ambiente: Mapped[Optional[float]]
-    luminosidad: Mapped[Optional[float]]
-    nitrogeno: Mapped[Optional[float]]
-    potasio: Mapped[Optional[float]]
-    fosforo: Mapped[Optional[float]]
-    fecha: Mapped[datetime] = mapped_column(db.DateTime)
-
-    planta: Mapped["Planta"] = relationship(back_populates="metricas")
-
-    def __repr__(self):
-        return f"""Metrica(
-        id_metrica={self.id_metrica!r},
-        id_planta={self.id_planta!r},
-        humedad_suelo={self.humedad_suelo!r},
-        temperatura_ambiente={self.temperatura_ambiente!r},
-        luminosidad={self.luminosidad!r},
-        nitrogeno={self.nitrogeno!r},
-        potasio={self.potasio!r},
-        fosforo={self.fosforo!r},
-        fecha={self.fecha!r}
-        )"""
-
-# Conexion con la db
-engine = db.create_engine(f"postgresql://{usuario}:{pwd}@{host}:5433/cultibotdb")
-
-# Crea las tablas
+# Crea las tablas del models si es que aún no han sido creadas
 Base.metadata.create_all(bind=engine)
-
-# Creamos una session
-session = Session(engine)
 
 # ------- Manejo de las peticiones -------
 app = FastAPI()
 
+# Generador de dependencia para abrir/cerrar la sesión en cada request
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
 # Recupera todas las plantas para la vista inicial
 @app.get("/")
-def todas_plantas():
-    query = db.select(Planta).order_by(Planta.id_planta.desc())
-    plantas = session.execute(query)
-    print(plantas)
-    with engine.connect() as conn:
-        plantas = conn.execute(query).mappings().fetchall()
-    plantas_list = [Planta.from_row(p) for p in plantas]
-    return {"plantas":plantas_list}
+def todas_plantas(session: Session = Depends(get_session)):
+    query = select(Planta).order_by(Planta.id_planta.desc())
+    plantas = session.scalars(query).all()
+    return plantas
 
 # Extrae todas las metricas de una planta para graficarlas
 @app.get("/metricas") # http://127.0.0.1:8000/metricas?id=1
-def metricas(id: int = 0):
+def metricas(id: int = 0, session: Session = Depends(get_session)):
     query = (
-        db.select(Planta, Metrica)
+        select(Planta, Metrica)
         .join(Metrica, Planta.c.id_planta == Metrica.c.id_planta)
         .where(Planta.c.id_planta == id)
         .order_by(Metrica.c.fecha.desc())
     )
-    with engine.connect() as conn:
-        plantas = conn.execute(query).mappings().fetchall()
+    plantas = session.scalars(query).all()
     
     if not plantas:
-        return {"error": "Planta no encontrada"}, 404
+        raise HTTPException(status_code=404, detail="Planta no encontrada")
     
-    metricas_list = [Metrica.from_joined_row(p) for p in plantas]
-    return {"metricas": metricas_list}
+    # metricas_list = [Metrica.from_joined_row(p) for p in plantas]
+    # return {"metricas": metricas_list}
+    return plantas
 
 # Recupera la ultima metrica para mostrarlas en las cards
 @app.get("/ultimaMetrica") # http://127.0.0.1:8000/ultimaMetrica?id=1
-def ultima_metrica(id: int = 0):
+def ultima_metrica(id: int = 0, session: Session = Depends(get_session)):
     query = (
-        db.select(Planta, Metrica)
+        select(Planta, Metrica)
         .join(Metrica, Planta.c.id_planta == Metrica.c.id_planta)
         .where(Planta.c.id_planta == id)
         .order_by(Metrica.c.fecha.desc())
         .limit(1)
     )
-    with engine.connect() as conn:
-        planta = conn.execute(query).mappings().first()
+    planta = session.scalar(query)
     
     if not planta:
-        return {"error": "Planta no encontrada"}, 404
+        raise HTTPException(status_code=404, detail="Planta no encontrada")
     
-    metrica_dto = Metrica.from_joined_row(planta)
-    return {"metrica": metrica_dto}
+    # metrica_dto = Metrica.from_joined_row(planta)
+    # return {"metrica": metrica_dto}
+    return planta
