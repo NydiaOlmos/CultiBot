4fd774a7977da39809aaccd1275ea7965f441f18
NydiaOlmos <nydia.naomi@gamil.com>
2026-08-03 19:43:00 -0600

    Fix: Cuando no existen métricas en todas las métricas retorna 204 en lugar de 404

--- backend/main.py
+++ backend/main.py
@@ -55,6 +55,10 @@ def todas_plantas(session: Session = Depends(get_session)):
 # http://127.0.0.1:8000/metricas?id=1
 @app.get("/metricas", status_code=200, response_model=PlantaConMetricasResponse) 
 def metricas(id: int = 0, session: Session = Depends(get_session)):
+    # Verifica que la planta exista
+    if not session.get(Planta, id):
+        raise HTTPException(status_code=404, detail="Planta no encontrada")
+    
     query = (
         select(Planta, Metrica)
         .join(Planta.metricas)
@@ -64,7 +68,7 @@ def metricas(id: int = 0, session: Session = Depends(get_session)):
     planta = session.scalar(query)
     
     if not planta:
-        raise HTTPException(status_code=404, detail="Planta no encontrada")
+        raise HTTPException(status_code=204, detail="La planta no tiene métricas")
     
     return planta
 
